cbf89bda463f205665cbde9a06822436dc7177d7
Dracozilong <Dracozilong@126.com>
2019-12-31 12:16:39 +0800

    修改订单状态 删除订单功能controller

--- foodie-dev-service/src/main/java/com/imooc/service/center/MyOrderService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/center/MyOrderService.java
@@ -1,5 +1,6 @@
 package com.imooc.service.center;
 
+import com.imooc.pojo.Orders;
 import com.imooc.utils.PagedGridResult;
 
 public interface MyOrderService {
@@ -13,4 +14,37 @@ public interface MyOrderService {
      * @return
      */
     public PagedGridResult queryMyOrders(String userId,Integer orderStatus,Integer page,Integer pageSize);
+
+
+    /**
+     * 根据订单Id 修改订单发货状态
+     * @param orderId
+     */
+    public void updateDeliverOrdersStatus(String orderId);
+
+
+    /**
+     * 查询我的订单
+     * @param userId
+     * @param orderId
+     * @return
+     */
+    public Orders queryMyorder(String userId,String orderId);
+
+    /**
+     * 更新订单状态 确认收货
+     * @param orderId
+     * @return
+     */
+    public boolean updateReceiveOrderStatus(String orderId);
+
+    /**
+     * 删除订单(逻辑删除)
+     * @param userId
+     * @param orderId
+     * @return
+     */
+    public boolean deleteOrder(String userId,String orderId);
+
+
 }

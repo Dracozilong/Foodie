ac23eff821c9c7a7b9ac14f468f25708dad8bc62
Dracozilong <Dracozilong@126.com>
2020-01-02 17:19:51 +0800

    商品评价保存 历史评价记录查询

--- foodie-dev-service/src/main/java/com/imooc/service/center/MyCommentsService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/center/MyCommentsService.java
@@ -1,6 +1,8 @@
 package com.imooc.service.center;
 
 import com.imooc.pojo.OrderItems;
+import com.imooc.pojo.bo.center.OrderItemsCommentBO;
+import com.imooc.utils.PagedGridResult;
 
 import java.util.List;
 
@@ -12,4 +14,17 @@ public interface MyCommentsService {
      * @return
      */
     public List<OrderItems>  queryPendingComment(String orderId);
+
+
+    /**
+     * 保存用户商品评价
+     * @param userId
+     * @param orderId
+     * @param commentList
+     */
+    public void saveComments(String userId, String orderId, List<OrderItemsCommentBO> commentList);
+
+
+    public PagedGridResult queryMyComments(String userId,Integer page,Integer pageSize);
+
 }

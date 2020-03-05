bed28f7088b72e607015381e4fe5fd063d2cea11
Dracozilong <Dracozilong@126.com>
2020-03-05 14:47:29 +0800

    本地环境回调地址

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -25,15 +25,18 @@ public class BaseController {
 
     //微信支付 -> 支付中心 -> 天天吃货平台
     //                      |-回调通知url
-    //String payReturnUrl ="http://api.zqiush.com:8088/foodie-dev-api/orders/notifyMerchantOrderPaid";
+    //  String payReturnUrl ="http://api.zqiush.com:8088/foodie-dev-api/orders/notifyMerchantOrderPaid";
 
-    //项目采用nginx前后端分离以后，要去除端口号，交给nginx进行转发
-    String payReturnUrl ="http://api.zqiush.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
+    //本地环境回调地址
+     String payReturnUrl ="http://foodieshop.natapp1.cc/orders/notifyMerchantOrderPaid";
+
+     //项目采用nginx前后端分离以后，要去除端口号，交给nginx进行转发
+    //String payReturnUrl ="http://api.zqiush.com/foodie-dev-api/orders/notifyMerchantOrderPaid";
 
 
     public static final String IMG_USER_FACE_LOCATION = "D:"+File.separator+"faces";
 
-//    public static final String IMG_USER_FACE_LOCATION = "D:\\faces";
+//   public static final String IMG_USER_FACE_LOCATION = "D:\\faces";
 
 
     public IMOOCJSONResult checkUserOrders(String userId, String orderId){
@@ -43,6 +46,7 @@ public class BaseController {
         if (order == null){
             IMOOCJSONResult.errorMsg("订单不存在");
         }
+
         return IMOOCJSONResult.ok(order);
     }
 }
